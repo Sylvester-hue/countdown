9f3f1e20636c7c03384a00372185f95dc669eed0
Sylvester Bockarie <148790716+Sylvester-hue@users.noreply.github.com>
2024-12-26 10:08:27 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 31, 2024 11:59:59").getTime();
+var countDownDate = new Date("Dec 31, 2024 23:59:59").getTime();
 
 // Update the countdown every 1 second
 var x = setInterval(function() {
